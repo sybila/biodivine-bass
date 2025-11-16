c84fd24784f5a1bab936135ee8e489897611871a
Samuel Pastva <daemontus@gmail.com>
2025-11-16 12:33:32 +0100

    More production ready benchmark scripts

--- benchmarks/bench_k++ADF.sh
+++ benchmarks/bench_k++ADF.sh
@@ -7,16 +7,18 @@ set -x
 
 mkdir -p ./results
 
-TIMEOUT='10s'
 TOOL='sybila/tool-k-adf'
 BENCHMARKS='./test_instances/_normalized'
+TIMEOUT=${TIMEOUT:-'10s'}
+PARALLEL=${PARALLEL:-'1'}
 
 # Time to one solution across various semantics.
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' -- adm
-for d in run_*/; do mv -- "$d" "results/yadf_1_adm_${d#./}"; done
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' -- com
-for d in run_*/; do mv -- "$d" "results/yadf_1_com_${d#./}"; done
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' -- prf
-for d in run_*/; do mv -- "$d" "results/yadf_1_prf_${d#./}"; done
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' -- stb
-for d in run_*/; do mv -- "$d" "results/yadf_1_stb_${d#./}"; done
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- adm
+for d in run_*/; do mv -- "$d" "results/k_adf_adm_${d#./}"; done
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- com
+for d in run_*/; do mv -- "$d" "results/k_adf_com_${d#./}"; done
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- prf
+for d in run_*/; do mv -- "$d" "results/k_adf_prf_${d#./}"; done
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- stb
+for d in run_*/; do mv -- "$d" "results/k_adf_stb_${d#./}"; done
+
--- benchmarks/bench_yadf.sh
+++ benchmarks/bench_yadf.sh
@@ -7,16 +7,17 @@ set -x
 
 mkdir -p ./results
 
-TIMEOUT='10s'
 TOOL='sybila/tool-yadf'
 BENCHMARKS='./test_instances/_normalized'
+TIMEOUT=${TIMEOUT:-'10s'}
+PARALLEL=${PARALLEL:-'1'}
 
 # Time to one solution across various semantics.
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' -- -adm 1
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- -adm 1
 for d in run_*/; do mv -- "$d" "results/yadf_1_adm_${d#./}"; done
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' -- -com 1
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- -com 1
 for d in run_*/; do mv -- "$d" "results/yadf_1_com_${d#./}"; done
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' -- -prf 1
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- -prf 1
 for d in run_*/; do mv -- "$d" "results/yadf_1_prf_${d#./}"; done
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' -- -stb 1
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- -stb 1
 for d in run_*/; do mv -- "$d" "results/yadf_1_stb_${d#./}"; done
